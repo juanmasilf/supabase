996a05268c9e3e105fe6e6bb4fd4bbb16da07677
juanmasilf <juanmasilveira@hotmail.com>
2022-10-17 19:53:58 -0300

    flutter - Supabase authentication example

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# supabase
+This repository is used for the Serverless Architecture Course at ORT University.
+
+## Exercises
+
+We use 2 libraries in order to do the exercises of the lessons, [React](https://reactjs.org/) and [Flutter](https://flutter.dev/).
+We divide the course in two, the second half using [Supabase](https://supabase.com/).
+
+Exercises are divided in folders depending on the library and language used.
+
+### flutter/authentication
+
+Lesson that consisted on explaining Supabase and the Supabase Authentication service.
---
+++ flutter/authentication/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_quickstart/pages/account_page.dart';
+import 'package:supabase_quickstart/pages/login_page.dart';
+import 'package:supabase_quickstart/pages/splash_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    // TODO: Replace credentials with your own
+    url: 'https://bciwqyntieprgypqlhxq.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjaXdxeW50aWVwcmd5cHFsaHhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU0MzkwNDAsImV4cCI6MTk4MTAxNTA0MH0.g_QaqC7WlZn9x73VKY9G0ifzeSoqDY6B68o1tBh3Afw',
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Supabase Flutter',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.green,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            onPrimary: Colors.white,
+            primary: Colors.green,
+          ),
+        ),
+      ),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (_) => const SplashPage(),
+        '/login': (_) => const LoginPage(),
+        '/account': (_) => const AccountPage(),
+      },
+    );
+  }
+}
