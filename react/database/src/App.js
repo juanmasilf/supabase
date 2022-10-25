1d6bf08ec63d51b4b206aa8d552a09bd8af8efa6
Tomas Piaggio <tomas@createthrive.com>
2022-10-25 17:12:51 -0300

    react/database

---
+++ react/database/src/App.js
@@ -0,0 +1,37 @@
+import './index.css'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
+import Auth from './auth/Auth'
+import Account from './Account'
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    });
+
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event === "PASSWORD_RECOVERY") {
+        const newPassword = prompt("What would you like your new password to be?");
+        const { data, error } = await supabase.auth
+          .updateUser({ password: newPassword })
+          
+        if (data) alert("Password updated successfully!")
+        if (error) alert("There was an error updating your password.")
+      }
+      setSession(session);
+    });
+  }, [])
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
+    </div>
+  )
+}
